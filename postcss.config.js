51767e12fefe0e6254f5cd6e2aee17d535a577c4
omohemma <emmaomoh@gmail.com>
2020-04-14 09:42:46 +0100

    Css Resolved

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     process.env.NODE_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
         // To specify the files to look for
-        content: ["./src/*/*.vue", "./public/index.html"],
+        content: ["./src/*/*.vue", "./src/*.vue", "./public/index.html"],
         // Regex to look out for classname in files
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
       })
